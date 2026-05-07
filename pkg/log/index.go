56ab358f5155b65c0996703266b2668cdd2e1d63
benliusf <benliusf@yahoo.com>
2026-05-06 21:01:52 -0700

    Minor

--- pkg/log/index.go
+++ pkg/log/index.go
@@ -6,29 +6,6 @@ import (
 
 const indexExt = ".idx"
 
-type indexIter struct {
-	idx *index
-	pos uint64
-}
-
-func (i *indexIter) hasNext() bool {
-	return i.pos < i.idx.size
-}
-
-func (i *indexIter) next() (hash uint64, pos uint64, err error) {
-	b := make([]byte, 8)
-	if _, err := i.idx.readAt(b, i.pos); err != nil {
-		return 0, 0, err
-	}
-	hash = enc.Uint64(b)
-	if _, err := i.idx.readAt(b, i.pos+8); err != nil {
-		return 0, 0, err
-	}
-	pos = enc.Uint64(b)
-	i.pos += 16
-	return hash, pos, nil
-}
-
 type index struct {
 	*store
 }
@@ -55,4 +32,27 @@ func (i *index) write(p []byte) (n uint64, pos uint64, err error) {
 	}
 	i.size += uint64(w)
 	return uint64(w), pos, nil
+}
+
+type indexIter struct {
+	idx *index
+	pos uint64
+}
+
+func (i *indexIter) hasNext() bool {
+	return i.pos < i.idx.size
+}
+
+func (i *indexIter) next() (hash uint64, pos uint64, err error) {
+	b := make([]byte, 8)
+	if _, err := i.idx.readAt(b, i.pos); err != nil {
+		return 0, 0, err
+	}
+	hash = enc.Uint64(b)
+	if _, err := i.idx.readAt(b, i.pos+8); err != nil {
+		return 0, 0, err
+	}
+	pos = enc.Uint64(b)
+	i.pos += 16
+	return hash, pos, nil
 }
